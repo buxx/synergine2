9f393fc9a9697e96d16758b5a8fbd86128dff34a
Bastien Sevajol <sevajol.bastien@gmail.com>
2016-11-09 20:56:55 +0100

    test processing with objects

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -5,19 +5,31 @@ from synergine2.utils import ChunkManager
 from tests import BaseTest
 
 
+class MyFakeClass(object):
+    def __init__(self, value):
+        self.value = value
+
+
 class TestProcessing(BaseTest):
     @staticmethod
-    def _make_job(data_chunk: list) -> tuple:
+    def _make_job_with_scalar(data_chunk: list) -> tuple:
         current_pid = os.getpid()
         result = sum(data_chunk)
         return current_pid, result
 
-    def test_parallel_jobs(self):
+    @staticmethod
+    def _make_job_with_object(data_chunk: list) -> tuple:
+        current_pid = os.getpid()
+        data = [o.value for o in data_chunk]
+        result = sum(data)
+        return current_pid, MyFakeClass(result)
+
+    def test_parallel_jobs_with_scalar(self):
         chunk_manager = ChunkManager(4)
         process_manager = ProcessManager(
             process_count=4,
             chunk_manager=chunk_manager,
-            job_maker=self._make_job,
+            job_maker=self._make_job_with_scalar,
         )
 
         data = list(range(100))
@@ -28,6 +40,31 @@ class TestProcessing(BaseTest):
 
         for process_id, result in results:
             final_result += result
+            process_id_list.append(process_id)
+
+        # Test each process ids are differents
+        assert sorted(process_id_list) == \
+            sorted(list(set(process_id_list)))
+
+        # Goal is 4950
+        assert final_result == 4950
+
+    def test_parallel_jobs_with_objects(self):
+        chunk_manager = ChunkManager(4)
+        process_manager = ProcessManager(
+            process_count=4,
+            chunk_manager=chunk_manager,
+            job_maker=self._make_job_with_object,
+        )
+
+        data = [MyFakeClass(v) for v in range(100)]
+        process_id_list = []
+        final_result = 0
+
+        results = process_manager.execute_jobs(data)
+
+        for process_id, result_object in results:
+            final_result += result_object.value
             process_id_list.append(process_id)
 
         # Test each process ids are differents
