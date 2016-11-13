5f2ac56cd54df36c9b432a834cbbe903438dcb76
Bastien Sevajol <sevajol.bastien@gmail.com>
2016-11-13 17:40:25 +0100

    add terminal test

--- tests/test_terminals.py
+++ tests/test_terminals.py
@@ -6,17 +6,23 @@ from synergine2.terminals import TerminalManager
 from tests import BaseTest
 
 
-class FakeTerminal(Terminal):
+class MultiplyTerminal(Terminal):
     def receive(self, package: TerminalPackage):
         self.send(TerminalPackage(package.value * 2))
         self.send(TerminalPackage(package.value * 4))
 
 
+class DivideTerminal(Terminal):
+    def receive(self, package: TerminalPackage):
+        self.send(TerminalPackage(package.value / 2))
+        self.send(TerminalPackage(package.value / 4))
+
+
 class TestTerminals(BaseTest):
-    def test_terminals_communications(self):
+    def test_terminal_communications(self):
         terminals_manager = TerminalManager(
             terminals=[
-                FakeTerminal(),
+                MultiplyTerminal(),
             ]
         )
         terminals_manager.start()
@@ -24,9 +30,10 @@ class TestTerminals(BaseTest):
 
         # We wait max 2s (see time.sleep) to consider
         # process have finished. If not, it will fail
+        packages = []
         for i in range(200):
-            packages = terminals_manager.receive()
-            if packages:
+            packages.extend(terminals_manager.receive())
+            if len(packages) == 2:
                 break
             time.sleep(0.01)
 
@@ -36,4 +43,31 @@ class TestTerminals(BaseTest):
         assert 168 in values
 
         terminals_manager.stop()  # pytest must execute this if have fail
-        # TODO: Tester avec plusieurs terminaux
+
+    def test_terminals_communications(self):
+        terminals_manager = TerminalManager(
+            terminals=[
+                MultiplyTerminal(),
+                DivideTerminal(),
+            ]
+        )
+        terminals_manager.start()
+        terminals_manager.send(TerminalPackage(42))
+
+        # We wait max 2s (see time.sleep) to consider
+        # process have finished. If not, it will fail
+        packages = []
+        for i in range(200):
+            packages.extend(terminals_manager.receive())
+            if len(packages) == 4:
+                break
+            time.sleep(0.01)
+
+        assert 4 == len(packages)
+        values = [p.value for p in packages]
+        assert 84 in values
+        assert 168 in values
+        assert 21 in values
+        assert 10.5 in values
+
+        terminals_manager.stop()  # pytest must execute this if have fail
