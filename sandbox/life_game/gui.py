a5e35ffd6b22a4c498f40e5c1a7fd89eb3353acc
Bastien Sevajol <sevajol.bastien@gmail.com>
2016-11-23 20:53:31 +0100

    lifegame better render

--- sandbox/life_game/gui.py
+++ sandbox/life_game/gui.py
@@ -1,14 +1,19 @@
 import cocos
+from cocos.actions import ScaleBy, Repeat, Reverse, RotateBy
+from cocos.director import director
 from cocos.layer import ScrollableLayer, Layer
 from cocos.sprite import Sprite
 from pyglet.window import key as wkey
+from random import randint
 
 from sandbox.life_game.simulation import CellDieEvent, Cell
 from sandbox.life_game.simulation import CellBornEvent
 from synergine2.gui import Gui
-from synergine2.simulation import Event
 from synergine2.terminals import TerminalPackage
 from synergine2.terminals import Terminal
+
+cell_scale = ScaleBy(1.1, duration=0.25)
+cell_rotate = RotateBy(360, duration=30)
 
 
 class Cells(Layer):
@@ -18,8 +23,10 @@ class Cells(Layer):
 
     def born(self, grid_position):
         cell = Sprite('resources/cells_l.png')
-        cell.position = grid_position[0] * 30 + 300, grid_position[1] * 30 + 300
+        cell.position = grid_position[0] * 30, grid_position[1] * 30
         cell.scale = 0.50
+        cell.do(Repeat(cell_scale + Reverse(cell_scale)))
+        cell.do(Repeat(cell_rotate + Reverse(cell_rotate)))
         self.cells[grid_position] = cell
         self.add(cell)
 
@@ -28,7 +35,7 @@ class Cells(Layer):
         del self.cells[grid_position]
 
 
-class MainScene(ScrollableLayer):
+class MainLayer(ScrollableLayer):
     is_event_handler = True
 
     def __init__(self):
@@ -36,13 +43,23 @@ class MainScene(ScrollableLayer):
 
         self.scroll_step = 20
 
-        self.sprite = Sprite('resources/banner-1711735_640.jpg')
-        self.sprite.position = 320, 240
-        self.sprite.opacity = 70
-        self.add(self.sprite, z=1)
+        self.background = Sprite('resources/banner-1711735_640.jpg')
+        self.background.position = 0, 0
+        self.background.opacity = 70
+        self.background.scale = 5
+        self.add(self.background, z=1)
 
         self.cells = Cells()
         self.add(self.cells)
+
+        self.cross = Sprite('resources/cross31x31.png')
+        self.cross.position = 0, 0
+        self.cross.opacity = 50
+        self.add(self.cross)
+
+        # Set scene center on center of screen
+        window_size = director.get_window_size()
+        self.position = window_size[0] // 2, window_size[1] // 2
 
     def on_key_press(self, key, modifiers):
         if key == wkey.LEFT:
@@ -66,8 +83,8 @@ class LifeGameGui(Gui):
     ):
         super().__init__(terminal, read_queue_interval)
 
-        self.background = MainScene()
-        self.main_scene = cocos.scene.Scene(self.background)
+        self.main_layer = MainLayer()
+        self.main_scene = cocos.scene.Scene(self.main_layer)
         self.positions = {}
 
         self.terminal.register_event_handler(CellDieEvent, self.on_cell_die)
@@ -81,14 +98,14 @@ class LifeGameGui(Gui):
             for subject in package.subjects:
                 if isinstance(subject, Cell):
                     self.positions[subject.id] = subject.position
-                    self.background.cells.born(subject.position)
+                    self.main_layer.cells.born(subject.position)
 
     def on_cell_die(self, event: CellDieEvent):
-        self.background.cells.die(self.positions[event.subject_id])
+        self.main_layer.cells.die(self.positions[event.subject_id])
 
     def on_cell_born(self, event: CellBornEvent):
         # TODO: La position peut evoluer dans un autre programme
         # resoudre cette problematique de données subjects qui évolue
         subject = self.terminal.subjects.get(event.subject_id)
         self.positions[event.subject_id] = subject.position
-        self.background.cells.born(subject.position)
+        self.main_layer.cells.born(subject.position)
