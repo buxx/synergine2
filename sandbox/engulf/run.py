bdaee593caab0f97cd2f0667b216e5919c26f2f9
Bastien Sevajol <sevajol.bastien@gmail.com>
2016-11-29 18:46:11 +0100

    engulf ideas

---
+++ sandbox/engulf/run.py
@@ -0,0 +1,20 @@
+"""
+
+Engulf is simulation containing:
+
+* Subjects who need to:
+  * eat
+    * low energy ground stuff
+    * other alive subjects
+    * other dead subjects
+  * sleep
+  * want to be not alone
+    * with non aggressive subjects
+  * want to be alone
+  * reproduce
+    * with non aggressive subjects
+    * and transmit tendencies because their cultures can be
+      * eat: - eat background stuff, + eat subjects
+      * alone/not alone: - be alone + not alone
+
+"""
