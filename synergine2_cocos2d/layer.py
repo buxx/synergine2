b75138f84728a3764de041973695a55ed12cfeb5
Bastien Sevajol <sevajol.bastien@gmail.com>
2017-06-27 19:36:31 +0200

    DEV

--- synergine2_cocos2d/layer.py
+++ synergine2_cocos2d/layer.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+import typing
+
 import cocos
 
 from synergine2.config import Config
@@ -22,7 +24,7 @@ class LayerManager(object):
 
         self.background_sprite = None  # type: cocos.sprite.Sprite
         self.ground_layer = None  # type: cocos.tiles.RectMapLayer
-        self.subject_layer = None  # type: cocos.layer.Layer
+        self.subject_layer = None  # type: cocos.tiles.RectMapLayer
         self.top_layer = None  # type: cocos.tiles.RectMapLayer
 
     def init(self) -> None:
@@ -34,13 +36,38 @@ class LayerManager(object):
 
         self.background_sprite = self.middleware.get_background_sprite()
         self.ground_layer = self.middleware.get_ground_layer()
-        self.subject_layer = cocos.layer.Layer()
+        self.subject_layer = self.get_subjects_layer()
         self.top_layer = self.middleware.get_top_layer()
 
         self.main_layer.add(self.background_sprite)
         self.main_layer.add(self.ground_layer)
         self.main_layer.add(self.subject_layer)
         self.main_layer.add(self.top_layer)
+
+    def get_subjects_layer(self) -> cocos.tiles.RectMapLayer:
+        # TODO: hardcoded values: use TMX infos
+        cells = self.generate_subjects_cells(width=70, height=40, tile_width=8, tile_height=8)
+        return cocos.tiles.RectMapLayer(
+            id='subject',
+            tw=8,  # hardcoded values: use TMX infos
+            th=8,  # hardcoded values: use TMX infos
+            cells=cells,
+        )
+
+    def generate_subjects_cells(
+        self,
+        width: int,
+        height: int,
+        tile_width: int,
+        tile_height: int,
+    ) -> typing.List[typing.List[cocos.tiles.RectCell]]:
+        cells = [[None] * height for x in range(width)]  # type: typing.List[typing.List[cocos.tiles.RectCell]]
+        for n in range(width * height):
+            i = n % width
+            j = height - (n // width + 1)
+            # TODO: C'est ici que l'on met une tile=XXX avec des subjects
+            cells[i][j] = cocos.tiles.RectCell(i, j, tile_width, tile_height, properties={}, tile=None)
+        return cells
 
     def center(self):
         self.background_sprite.set_position(
