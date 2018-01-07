5176d1afe3cfa0c28601076e02dcfb8971792bbd
Bastien Sevajol <sevajol.bastien@gmail.com>
2018-01-07 15:35:01 +0100

    add some test structures

---
+++ tests/test_simulation.py
@@ -0,0 +1,23 @@
+from tests import BaseTest
+
+
+class TestBehaviours(BaseTest):
+    def test_behaviour_produce_data(self):
+        pass
+
+    def test_behaviour_timebase(self):
+        pass
+
+    def test_behavior_not_called_if_no_more_subjects(self):
+        pass
+
+
+class TestMechanisms(BaseTest):
+    def test_mechanism_called_once_for_multiple_behaviors(self):
+        pass
+
+    def test_mechanism_not_called_if_no_behavior(self):
+        pass
+
+    def test_mechanism_not_called_if_behavior_timebase_not_active_yet(self):
+        pass
